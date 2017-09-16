a76e61938927cf5fa4ddca9114b539ae902ae136
Carlos Conde <carconper@gmail.com>
2017-09-16 22:01:56 +0000

    Some cleanup

--- app/filinger.py
+++ app/filinger.py
@@ -28,19 +28,14 @@ def metadata():
     try:
         r = requests.get(API_COMPANIES_EDGARONLINE, params=params)
     except requests.exceptions.RequestException as e:
-        # Save the backtrace info
         exc_info = sys.exc_info()
         logger.error('Get Company Metadata: %s -- %s',
                      str(e))
-        #raise EdgarRequestError, EdgarRequestError(e), exc_info[2]
-
     print("Company Metadata:", r.text, file=sys.stderr)
-
     info = json.loads(r.text)
     if info['result']['totalrows'] == 0:
         print("Company doesnt exist", file=sys.stderr)
         return jsonify(status='NOK', company_data=[])
-
     print("Company Metadata:", info['result']['rows'][0]['values'], file=sys.stderr)
     info = info['result']['rows'][0]['values']
     print("Company Metadata:", info, file=sys.stderr)
@@ -48,7 +43,6 @@ def metadata():
     for elem in info:
         aux[elem['field']] = elem['value']
         print(elem['field'], elem['value'], file=sys.stderr)
-
     print("Extracted data:", aux, file=sys.stderr)
 
     return jsonify(status="OK", company_data=aux)
@@ -65,7 +59,6 @@ def filings():
         'start': params['begin'],
         'count': params['count']
     }
-
     try:
         r = requests.get(SEC_URL_FILINGS, params=url_params)
         print("Response Headers: ", r.headers, file=sys.stderr)
@@ -73,31 +66,17 @@ def filings():
         exc_info = sys.exc_info()
         logger.error('Get Company Metadata: %s -- %s',
                      str(e))
-        #raise SecRequestError, SecRequestError(e), exc_info[2]
-
     if int(r.headers['content-length']) < 2000:
         print("Company doesnt exist or doesnt have filings", file=sys.stderr)
         return jsonify(status="NOK", filing_list=filings)
-
     tree = html.fromstring(r.content)
-    #print("The text", r.text)
-    #filings = tree.xpath('//td[@nowrap="nowrap"]/text()')
-    #print("The filings: ", filings, file=sys.stderr)
-
-    #test = [td.text_content() for td in tree.xpath('//td')]
-    #print("The table contents: ", test, file=sys.stderr)
-    #filings = []
     documents = tree.xpath('//a[@id="documentsbutton"]/@href')
     print("The documents: ", documents, file=sys.stderr)
 
-
     df = pd.read_html(r.content,attrs = {'class': 'tableFile2'})[0]
-    #df2 = pd.read_html(r.content,attrs = {'class': 'tableFile2'})
-
     forms = df[0].tolist()
     descriptions = df[2].tolist()
     dates = df[3].tolist()
-
     for i in range(1, len(forms)):
         tmp = {
                 'form': forms[i],
